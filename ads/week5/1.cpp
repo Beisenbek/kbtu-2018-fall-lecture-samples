c3238735ddad7f41333ebe6544c968296f9f02dc
bsnbk <beysenbek@gmail.com>
2018-09-29 12:06:50 +0600

    upd

---
+++ ads/week5/1.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+
+using namespace std;
+
+struct node{
+    int val;
+    node * left;
+    node * right;
+    int level;
+    node(int x){
+        val = x;
+        level = -1;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+node * add(node * root, int x){
+    node *n = new node(x);
+    if(root == NULL){
+        root = n;
+        root->level = 0;
+    }else{
+        if(x < root->val){
+            if(root->left == NULL){
+                root->left = n;
+                root->left->level = root->level + 1;
+            }else{
+                add(root->left, x);
+            }
+        }else if(x > root->val){
+            if(root->right == NULL){
+                root->right = n;
+                root->right->level = root->level + 1;
+            }else{
+                add(root->right, x);
+            }
+        }
+   }
+   return root;
+}
+
+node * find(int x){
+
+}
+
+int rightmost(node * root){
+    if(root->right == NULL) return root->val;
+    rightmost(root->right);
+}
+
+node * del(node * root, int x){
+    if(root == NULL) return root;
+    if(root->val == x){
+        if(root->right == NULL && root->left == NULL){
+            return NULL;
+        }
+        if(root->right != NULL && root->left == NULL){
+            node * temp = root->right;
+            //delete root;
+            return temp;
+        }
+        if(root->right == NULL && root->left != NULL){
+            node * temp = root->left;
+            //delete root;
+            return temp;
+        }
+        root->val = rightmost(root->left);
+        root->left = del(root->left, root->val);
+        return root;
+    }
+
+    if(x < root->val){
+        root->left = del(root->left, x);
+        return root;
+    }
+
+    if(x > root->val){
+        root->right = del(root->right, x);
+        return root;
+    }
+}
+
+void print(node * n){
+    if(n == NULL) return;
+    cout << n->val << " "  << n->level << endl;
+    print(n->left);
+    print(n->right);
+}
+
+int main(){
+
+    node * root = NULL;
+
+    int d[] = {7, 3, 2, 5, 4, 8};
+
+    for(int i = 0; i < 6; ++i){
+        root = add(root, d[i]);
+    }
+
+    print(root);
+
+    del(root, 7);
+
+    cout << "=============" << endl;
+
+    print(root);
+
+    return 0;
+}
